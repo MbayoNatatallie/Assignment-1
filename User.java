1bbf0f260f13ff2cb0ceac6004865adea3d6f330
Natallie M'bayo <natalliembayo@Natallies-iMac.local>
2021-10-18 03:06:27 -0700

    Username generator

---
+++ User.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+import java.util.Random;
+
+public class User {
+
+    public static void main (String[] args){
+
+       String firstName;
+
+       String lastName;
+
+       // create a new scanner to collect user input 
+       Scanner userInput = new Scanner(System.in);
+
+       // Asks user for their first name
+       System.out.println("Enter your Firstname: "); 
+
+       // To collect and store firstname of the user's input
+       firstName = userInput.nextLine();
+
+       System.out.println("You entered: " + firstName); 
+
+       // Asks user for their lastname
+       System.out.println("Now enter your Last name: ");
+
+       // To collect and store lastname of the user's input
+       lastName = userInput.nextLine();
+
+       System.out.println("You entered: " + lastName); 
+       // Prints out user input of their last name they've entered
+
+       System.out.println("Your name is : " + firstName + " " + lastName); // Contain user fullname
+
+
+        char userfirstInitial = firstName.charAt(0);// Contain letter of user's first name
+
+        System.out.println("The first Initial of your firstname is: " + userfirstInitial ); 
+        // Print a string composed of the first letter of the user’s first name
+
+        if (lastName != null && lastName.length() > 5) {
+            System.out.println("First five characters of your lastname: " + lastName.substring(0, 5)); 
+            System.out.println(userfirstInitial + lastName.substring(0, 5));
+            // Prints first five characters of the user’s last name
+        }else {
+            System.out.println("Sorry! Last name does not contain up to five character.");
+            // If username is not up to five characters elae condition prompt this error
+        }
+
+        Random rand = new Random(); // To creates random numbers from 10 to 99
+
+        int num1 = 10;
+
+        num1 = rand.nextInt(89) + 10;
+        System.out.println("From 10 to 99: " + num1);
+        // followed by a random number in the range 10 to 99
+
+        System.out.println("Produces random numbers from 10 to 99: " + num1);
+        System.out.println("Username: " + userfirstInitial + lastName.substring(0, 5) + num1);
+
+
+    }
+}
